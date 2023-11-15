5ecf6532495abfa8a9b47aa9ef08893ba7112c5b
StankovicStefan17 <128034154+StankovicStefan17@users.noreply.github.com>
2023-11-15 23:06:18 +0100

    Day 5

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+/*
 //mark
 const markHeight = 1.90; //cm
 const markMass = 99; //kg
@@ -46,3 +47,82 @@ let n = "1" + 1;
 n = n - 1;
 
 console.log(n);
+
+
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log("Sarha can start driving license🐱‍🚀")
+}
+else {
+    const yearsLeft = 18 - age;
+    console.log(`sarah is too young. Wait for ${yearsLeft} years`);
+}
+*/
+/*
+const age = 18
+if (age === 18) console.log(`You just became an adult :D`)
+*/
+/*
+const favorite = Number(prompt("What's your favourite number?"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+    console.log(`Cool! 23 is amazing number!`)
+} else if (favorite == 7) {
+    console.log(`7 is cool number`)
+}
+
+if (favorite !== 23) {
+    console.log("nije 23");
+}
+*/
+// Coding Challenge #3 Task 1,2,3,4
+
+const DolphinsFirstScore = 97;
+const DolphinsSecondScore = 112;
+const DolphinsThirdScore = 101;
+
+
+
+const KoalasFirstScore = 109;
+const KoalasSecondScore = 95;
+const KoalasThirdScore = 106;
+
+const DolphinsAvrageScore = (DolphinsFirstScore + DolphinsSecondScore + DolphinsThirdScore) / 3
+
+const KoalasAvrageScore = (KoalasFirstScore + KoalasSecondScore + KoalasThirdScore) / 3
+
+/* //task 1,2
+if (DolphinsAvrageScore > KoalasAvrageScore) {
+    console.log(`Dolphins wins with avarage score ${DolphinsAvrageScore}`);
+} else if (KoalasAvrageScore > DolphinsAvrageScore) {
+    console.log(`Koalas wins with avarage score ${KoalasAvrageScore}`);
+}
+else {
+    console.log(`It's draw both teams have same score: ${KoalasAvrageScore}`);
+}
+*/
+
+// //taks
+// if (DolphinsAvrageScore > KoalasAvrageScore && DolphinsAvrageScore >= 100) {
+//     console.log(`Dolphins wins with avarage score ${DolphinsAvrageScore}`);
+// } else if (KoalasAvrageScore > DolphinsAvrageScore && KoalasAvrageScore >= 100) {
+//     console.log(`Koalas wins with avarage score ${KoalasAvrageScore}`);
+// }
+// else if (KoalasAvrageScore === DolphinsAvrageScore && KoalasAvrageScore >= 100) {
+//     console.log(`It's draw both teams have same score: ${KoalasAvrageScore}`);
+// }
+// else {
+//     console.log(`No team wins the tophy`);
+// }
+
+const age = 23;
+age >= 50 ? console.log(`i like to drink wine 🎉`) : console.log(`i like to drink wine 🎂`);
+
+
+const drink = age >= 18 ? `wine` : `water`;
+console.log(drink);
