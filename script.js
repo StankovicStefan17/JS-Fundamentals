5599210c1d9a52555c55c03d97c16ad588d0bf00
StankovicStefan17 <128034154+StankovicStefan17@users.noreply.github.com>
2023-11-25 22:51:45 +0100

    Day 8
    05- 08

--- script.js
+++ script.js
@@ -169,3 +169,31 @@ console.log(`avion`);
 const calcAge = birthYhear => 2037 - birthYhear;
 
 // LIstening lecture 006
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const temperatures1 = [5, -4, -3, -1, 'error', 9, 13, 17, 15, 14, 25, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitude = calcTempAmplitude(temperatures);
+
+console.log(amplitude);
+
+const mergedTemps = temperatures.concat(temperatures1);
+
+console.log(calcTempAmplitude(mergedTemps));
