1be4bae22f58c64c4f56d27bf57c593f16c415ca
StankovicStefan17 <128034154+StankovicStefan17@users.noreply.github.com>
2023-12-21 18:33:57 +0100

    Day 34
    Maps

--- script.js
+++ script.js
@@ -226,26 +226,67 @@ for (const [team, odd] of Object.entries(game.odds)) {
 // }
 // console.log('scorers ' + Object.keys(scorers) + Object.values(scorers));
 
-const ordersSet = new Set(['Pasta', 'pica', 'Pasta', 'rizoto']);
+// const ordersSet = new Set(['Pasta', 'pica', 'Pasta', 'rizoto']);
 
-console.log(ordersSet);
+// console.log(ordersSet);
 
-console.log(new Set('Stefan'));
+// console.log(new Set('Stefan'));
 
-console.log(ordersSet.size);
-console.log(ordersSet.has('pica'));
-console.log(ordersSet.has('pica1'));
-ordersSet.add('bread');
-ordersSet.delete('rizoto');
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('pica'));
+// console.log(ordersSet.has('pica1'));
+// ordersSet.add('bread');
+// ordersSet.delete('rizoto');
 
-console.log(ordersSet);
+// console.log(ordersSet);
 
-for (const order of ordersSet) console.log(order);
+// for (const order of ordersSet) console.log(order);
 
-// Example;
+// // Example;
 
-const staff = ['Waiter', 'Chef', 'Waiter', 'Chef', 'Manager'];
+// const staff = ['Waiter', 'Chef', 'Waiter', 'Chef', 'Manager'];
 
-const staffSet = [...new Set(staff)];
-console.log(staffSet);
-console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Chef', 'Manager']).size);
+// const staffSet = [...new Set(staff)];
+// console.log(staffSet);
+// console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Chef', 'Manager']).size);
+
+const rest = new Map();
+
+rest.set('name', 'calsicco');
+rest.set(1, 'firenze');
+rest.set(2, 'lisabon');
+
+console.log(rest);
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'we are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+
+// rest.get(time > rest.get(open) && time < rest.get('close'));
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+rest.clear();
+console.log(rest.size);
+
+const arr = [1, 2];
+rest.set(arr, 'test');
+console.log(rest);
+
+rest.get([1, 2]);
+console.log(rest.get(arr));
+
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
