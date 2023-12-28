6d4d2cb346ab106f7686ab95cbd3b5ea42e0663d
StankovicStefan17 <128034154+StankovicStefan17@users.noreply.github.com>
2023-12-28 19:11:00 +0100

    Day 2

--- script.js
+++ script.js
@@ -568,25 +568,68 @@ const plane = `A320`;
 // capitalizName('Alisa strim');
 // capitalizName('stfan stankovic sasa dragica una paja');
 
-const mes = 'go to gate 23';
-console.log(mes.padStart(25, '+').padEnd(35, '+'));
+// const mes = 'go to gate 23';
+// console.log(mes.padStart(25, '+').padEnd(35, '+'));
 
-const maskCreditCard = function (number) {
-  const str = number + '';
-  const last = str.slice(-4);
-  return last.padStart(str.length, '*');
+// const maskCreditCard = function (number) {
+//   const str = number + '';
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// };
+
+// console.log(maskCreditCard(43385468756354));
+// console.log(maskCreditCard(4546487981111));
+
+// //Repeat
+// const message2 = 'Bad weather.. All departues Delayed... ';
+
+// console.log(message2.repeat(5));
+
+// const planeInLine = function (n) {
+//   console.log(`There are ${n} planes in line`.repeat(n));
+// };
+
+// planeInLine(4);
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+const button = document.querySelector('button');
+
+const convertinCamelCase = function () {
+  const text = document.querySelector('textarea').value;
+  console.log(text);
+  console.log('kliknuto');
 };
 
-console.log(maskCreditCard(43385468756354));
-console.log(maskCreditCard(4546487981111));
-
-//Repeat
-const message2 = 'Bad weather.. All departues Delayed... ';
-
-console.log(message2.repeat(5));
-
-const planeInLine = function (n) {
-  console.log(`There are ${n} planes in line`.repeat(n));
-};
-
-planeInLine(4);
+button.addEventListener(`click`, convertinCamelCase);
