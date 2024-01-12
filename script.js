a9cd607e9c64754b7d67a591af3645c54dd54c19
StankovicStefan17 <128034154+StankovicStefan17@users.noreply.github.com>
2024-01-12 16:24:26 +0100

    Day 16

--- script.js
+++ script.js
@@ -117,62 +117,64 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
-console.log(arr.slice(3, 4));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice(1, -1));
-console.log(arr.slice());
-console.log(arr.slice([...arr]));
+// console.log(arr.slice(3, 4));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice(1, -1));
+// console.log(arr.slice());
+// console.log(arr.slice([...arr]));
 
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// console.log(arr);
 
-arr.splice();
+// arr.splice();
 
-console.log(arr.reverse());
-console.log(arr);
+// console.log(arr.reverse());
+// console.log(arr);
 
-const arr2 = arr;
-const letters = arr.concat(arr2);
+// const arr2 = arr;
+// const letters = arr.concat(arr2);
 
-console.log(letters);
+// console.log(letters);
 
-///join
-console.log(letters.join('-'));
+// ///join
+// console.log(letters.join('-'));
 
-const arr23 = [23, 11, 43];
-console.log(arr23[0]);
-console.log(arr23.at(0));
+// const arr23 = [23, 11, 43];
+// console.log(arr23[0]);
+// console.log(arr23.at(0));
 
-console.log(arr23[arr23.length - 1]);
-console.log(arr23.slice(-1));
+// console.log(arr23[arr23.length - 1]);
+// console.log(arr23.slice(-1));
 
-console.log(arr23.at(-1));
-console.log('arr23'.at(-1));
+// console.log(arr23.at(-1));
+// console.log('arr23'.at(-1));
 
-for (const movement of movements) {
-  if (movement > 0) {
-    console.log(`You deposited ${movement}`);
-  } else {
-    console.log(`you withdrew ${Math.abs(movement)}`);
-  }
-}
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`you withdrew ${Math.abs(movement)}`);
+//   }
+// }
 
-console.log('-------');
-movements.forEach(function (movement1, i) {
-  if (movement1 > 0) {
-    console.log(`${i}: You deposited ${movement1}`);
-  } else {
-    console.log(`${i}: you withdrew ${Math.abs(movement1)}`);
-  }
-});
+// console.log('-------');
+// movements.forEach(function (movement1, i) {
+//   if (movement1 > 0) {
+//     console.log(`${i}: You deposited ${movement1}`);
+//   } else {
+//     console.log(`${i}: you withdrew ${Math.abs(movement1)}`);
+//   }
+// });
 
-currencies.forEach(function (value, key, map) {
-  console.log(value, key, map);
-});
+// currencies.forEach(function (value, key, map) {
+//   console.log(value, key, map);
+// });
 
-const currenciesUniq = new Set(['USD', 'GBP', 'USD', 'EUR']);
-currenciesUniq.forEach(function (value, key, map) {
-  console.log(value, key, map);
-});
+// const currenciesUniq = new Set(['USD', 'GBP', 'USD', 'EUR']);
+// currenciesUniq.forEach(function (value, key, map) {
+//   console.log(value, key, map);
+// });
+
+//start of BANKIST app
