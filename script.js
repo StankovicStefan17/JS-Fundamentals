a656cf68dcba892a409dcac1a3ac5bfd3c3ec0c5
StankovicStefan17 <128034154+StankovicStefan17@users.noreply.github.com>
2024-01-26 22:46:51 +0100

    Day 1

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ const displayMovments = function (movements) {
     const html = `
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov}€</div>
     </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -126,6 +126,27 @@ const calcPrintBalance = function (movements) {
 };
 
 calcPrintBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, move) => acc + move);
+
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const intrest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter(int => int >= 1)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${intrest}€`;
+};
+calcDisplaySummary(account1.movements);
 
 const user = `Steven Thomas Williams`; //stw
 
